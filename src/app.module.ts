1001ec02beae42a211502b2faee3f6d0966c4bd9
Joao Lopes <jp.lopes.melo01@gmail.com>
2026-09-12 17:57:59 -0300

    integração html

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { ClienteModule } from './cliente/cliente.module.js';
 import { MedicoModule } from './medico/medico.module.js';
 import { AdmModule } from './adm/adm.module.js';
 import { ConsultasModule } from './consultas/consultas.module.js';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [PrismaModule, ClienteModule, MedicoModule, AdmModule, ConsultasModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Transforma sua pasta 'html' na raiz do seu site
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'html'),
+    }),
+    PrismaModule,
+    ClienteModule,
+    MedicoModule,
+    AdmModule,
+    ConsultasModule,
+  ],
+  controllers: [], // Deixamos vazio, não precisamos mais do AppController
+  providers: [],   // Deixamos vazio, não precisamos mais do AppService
 })
 export class AppModule {}
